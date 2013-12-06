5ecc380abc4b6122873f79812d9f682e81f31f9a
Kory Nunn <knunn187@gmail.com>
2013-12-07 00:28:39 +1000

    Added a -j option to eval stuff with

--- jspect.js
+++ jspect.js
@@ -1,27 +1,46 @@
 #! /usr/bin/env node
 
-if(!process.argv[2]){
-    console.warn('jspect needs a target! eg: "jspect ./package.json"');
-    return;
+function runWith(object, code){
+    return eval(code.toString());
 }
 
-var program = require('commander'),
-    path = require('path'),
-    packageJson = require('./package.json'),
-    Gedi = require('gedi'),
-    object = require(path.resolve(process.argv[2])),
-    gedi = new Gedi(object);
+function jspect(){
 
-program
-  .version(packageJson.version)
-  .option('-p, --property [propertyPath]', 'Return the value of the object at this path.')
-  .option('-e, --expression [expression]', 'Use a gedi/gel expression.')
-  .parse(process.argv);
+    var program = require('commander'),
+        path = require('path'),
+        packageJson = require('./package.json'),
+        Gedi = require('gedi'),
+        objectPath = process.argv[2],
+        object,
+        gedi;
 
-if(program.expression){
-    console.log(gedi.get(program.expression));
-}else if(program.property){
-    console.log(gedi.get(gedi.paths.create(program.property.split('.'))));
-}else{
-    console.log(object);
+    if(objectPath && objectPath.indexOf('-') !== 0){
+        object = require(path.resolve(objectPath)),
+        gedi = new Gedi(object);
+    }
+
+
+    program
+      .version(packageJson.version)
+      .option('-p, --property [propertyPath]', 'Return the value of the object at this path.')
+      .option('-e, --expression [expression]', 'Use a gedi/gel expression.')
+      .option('-j, --javascript [javascript]', 'Pretty much just eval')
+      .parse(process.argv);
+
+    if(!object){
+        console.warn('jspect needs a target file to inspect, eg: jspect ./package.json');
+        return;
+    }
+
+    if(program.expression){
+        console.log(gedi.get(program.expression));
+    }else if(program.property){
+        console.log(gedi.get(gedi.paths.create(program.property.split('.'))));
+    }else if(program.javascript){
+        console.log(runWith(object, program.javascript));
+    }else{
+        console.log(object);
+    }
 }
+
+jspect();
